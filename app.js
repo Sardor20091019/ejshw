1ebd244e9c6657b06b39e2f619394ea911c98a92
Sunnatullayev Sardor <98585642+Sardor20091019@users.noreply.github.com>
2025-12-27 14:42:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+// TASK 1
+app.get("/task1", (req, res) => {
+  const courses = ["JavaScript", "Node.js", "Express", "MongoDB"];
+  res.render("task1-courses", { courses });
+});
+
+// TASK 2
+app.get("/task2", (req, res) => {
+  const score = 78;
+  res.render("task2-score", { score });
+});
+
+// TASK 3
+app.get("/task3", (req, res) => {
+  const users = [
+    { id: 1, name: "Ali", age: 20 },
+    { id: 2, name: "Vali", age: 25 },
+    { id: 3, name: "Sami", age: 18 }
+  ];
+  res.render("task3-users", { users });
+});
+
+// TASK 4
+let students = [];
+
+app.get("/task4", (req, res) => {
+  res.render("task4-students", { students, error: null });
+});
+
+app.post("/task4", (req, res) => {
+  const { name, age } = req.body;
+
+  if (!name || !age) {
+    return res.render("task4-students", {
+      students,
+      error: "Barcha maydonlarni to‘ldiring!"
+    });
+  }
+
+  students.push({ name, age: Number(age) });
+  res.redirect("/task4");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
